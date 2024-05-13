5d9d662e3a4aa01b1335680b99fa1ac7ba445aac
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-05-13 14:39:01 +0600

    connect server with mongodb and create an get api for jobs data

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app=express();
 const port=process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7nkbk6a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const jobsCollection=client.db("jobsPlacerDB").collection("jobs");
+
+    app.get("/jobs", async(req,res)=>{
+        const result=await jobsCollection.find().toArray();
+        res.send(result);
+    })
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
 
 app.get("/", async(req,res)=>{
     res.send("JobsPlacer server is running perfectly")
