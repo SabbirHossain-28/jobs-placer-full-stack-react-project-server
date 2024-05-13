273923417438dbe02178439c660d0c5eeebfcc08
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-05-13 22:02:34 +0600

    create dynamic get api for id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app=express();
 const port=process.env.PORT || 5000;
@@ -29,6 +29,12 @@ async function run() {
         const result=await jobsCollection.find().toArray();
         res.send(result);
     })
+     app.get("/jobDetails/:id",async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:new ObjectId(id)};
+        const result=await jobsCollection.findOne(query);
+        res.send(result)
+     })
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
